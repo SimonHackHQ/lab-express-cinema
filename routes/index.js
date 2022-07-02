b42d8c3b8ebce2abd996b6e7df3269e752334688
unknown <Jossuha@hotmail.fr>
2022-07-02 16:58:29 +0200

    Finish

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,23 @@ router.get('/', (req, res, next) => res.render('index'));
 router.get('/movies', (req, res, next)=>{
     Movie.find()
         .then(moviesFromDB => {
-            console.log("Retrieved movies from DB", moviesFromDB);
+            //console.log("Retrieved movies from DB", moviesFromDB);
             
             res.render("movies", { movies: moviesFromDB });
         })
         .catch(error => console.log("Unable to get data from DB: ", error));
+})
+
+router.get('/movies/:id', (req, res, next)=>{
+    Movie.findById(req.params.id)
+    .then(movieFromDB =>{
+        console.log(movieFromDB)
+        res.render("details", { 
+            title: movieFromDB.title,
+            movie: movieFromDB 
+        });
+    })
+    .catch(err => console.log('Could not finding id',err))
 })
 
 module.exports = router;
